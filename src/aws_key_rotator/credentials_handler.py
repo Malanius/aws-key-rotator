57e7710b2824feee01c74b14bb6981bfb8214cfa
Malanius Privierre <malaniusprivierre@gmail.com>
2022-03-22 20:02:04 +0100

    ✨ (profiles): Save new profile credentials

--- src/aws_key_rotator/credentials_handler.py
+++ src/aws_key_rotator/credentials_handler.py
@@ -24,3 +24,12 @@ def get_profile_credentials(profile_name: str, credentials_path: str) -> Credent
         "key_id": config.get(profile_name, "aws_access_key_id"),
         "secret_key": config.get(profile_name, "aws_secret_access_key")
     }
+
+
+def update_profile_credentials(key_id: str, secret_key: str, profile_name: str, credentials_path: str) -> None:
+    print(f"Updating credentials for profile: {profile_name}")
+    config.set(profile_name, "aws_access_key_id", key_id)
+    config.set(profile_name, "aws_secret_access_key", secret_key)
+    with open(credentials_path, 'w') as configfile:
+        config.write(configfile)
+    print(f"Updated credentials for profile: {profile_name}")
